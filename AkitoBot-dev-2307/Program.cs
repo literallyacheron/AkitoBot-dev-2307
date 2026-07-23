14f90e2faaa8f472437207c21b207dee8c91012e
sisib <sisib@CHRISTIAN-WIN11>
2026-07-23 04:17:34 +0200

    scrap that

---
+++ AkitoBot-dev-2307/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Hosting;
+using NetCord.Hosting.Gateway;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// Adds the Discord gateway service to your application host
+builder.Services.AddDiscordGateway();
+
+var host = builder.Build();
+
+await host.RunAsync();
